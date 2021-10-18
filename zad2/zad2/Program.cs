b0b7b750aacddc039846262773aada6c384afc4f
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-10-18 09:17:29 +0200

    Add files via upload

---
+++ zad2/zad2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zrakoplov x380 = new Zrakoplov("Airbus", 2000, 6000);
+            Zrakoplov c162 = new Zrakoplov("Cessna Skycatcher", 75, 870);
+            Zrakoplov pc_21 = new Zrakoplov("Pilatus", 1200, 1333);
+        }
+    }
+}
